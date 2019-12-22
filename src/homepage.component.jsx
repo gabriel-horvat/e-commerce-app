583ea608ef85fa0ebb5241b872b4595a65916c48
Gabmeister <gabriel@gabrielhorvat.com>
2019-12-22 10:48:47 -0500

    basic home page

---
+++ src/homepage.component.jsx
@@ -0,0 +1,69 @@
+import React from 'react'
+
+export const HomePage = () => {
+    return (
+        <div className = 'homepage'>
+            <div className = 'directory-menu'>
+
+                <div className = 'menu-item'>
+                    <div className = 'content'>
+                        <h1 className = 'title'>
+                            HATS
+                        </h1>
+                        <span className = 'subtitle'>
+                          SHOP NOW  
+                            </span>
+                    </div>
+                </div>
+
+                <div className = 'menu-item'>
+                    <div className = 'content'>
+                        <h1 className = 'title'>
+                            JACKETS
+                        </h1>
+                        <span className = 'subtitle'>
+                          SHOP NOW  
+                            </span>
+                    </div>
+                </div>
+
+                <div className = 'menu-item'>
+                    <div className = 'content'>
+                        <h1 className = 'title'>
+                            WOMANS
+                        </h1>
+                        <span className = 'subtitle'>
+                          SHOP NOW  
+                            </span>
+                    </div>
+                </div>
+
+                <div className = 'menu-item'>
+                    <div className = 'content'>
+                        <h1 className = 'title'>
+                            MENS
+                        </h1>
+                        <span className = 'subtitle'>
+                          SHOP NOW  
+                            </span>
+                    </div>
+                </div>
+
+                <div className = 'menu-item'>
+                    <div className = 'content'>
+                        <h1 className = 'title'>
+                            HATS
+                        </h1>
+                        <span className = 'subtitle'>
+                          SHOP NOW  
+                            </span>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    )
+}
+
+export default HomePage 
