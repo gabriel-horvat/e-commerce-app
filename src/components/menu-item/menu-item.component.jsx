6295e2f9081651a4a21b0089119b71c297051052
Gabmeister <gabriel@gabrielhorvat.com>
2020-01-19 17:41:51 +0100

    split up code, make it DRYer

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import './menu-item.styles.scss'
+
 
 const MenuItem = ({title}) => (
     <div className = 'menu-item'>
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,68 +1,10 @@
 import React from 'react'
-import './homepage.styles.scss'
+import Directory from '../../components/directory/directory.component'
 
 export const HomePage = () => {
     return (
         <div className = 'homepage'>
-            <div className = 'directory-menu'>
-
-                <div className = 'menu-item'>
-                    <div className = 'content'>
-                        <h1 className = 'title'>
-                            HATS
-                        </h1>
-                        <span className = 'subtitle'>
-                          SHOP NOW  
-                            </span>
-                    </div>
-                </div>
-
-                <div className = 'menu-item'>
-                    <div className = 'content'>
-                        <h1 className = 'title'>
-                            JACKETS
-                        </h1>
-                        <span className = 'subtitle'>
-                          SHOP NOW  
-                            </span>
-                    </div>
-                </div>
-
-                <div className = 'menu-item'>
-                    <div className = 'content'>
-                        <h1 className = 'title'>
-                            WOMANS
-                        </h1>
-                        <span className = 'subtitle'>
-                          SHOP NOW  
-                            </span>
-                    </div>
-                </div>
-
-                <div className = 'menu-item'>
-                    <div className = 'content'>
-                        <h1 className = 'title'>
-                            MENS
-                        </h1>
-                        <span className = 'subtitle'>
-                          SHOP NOW  
-                            </span>
-                    </div>
-                </div>
-
-                <div className = 'menu-item'>
-                    <div className = 'content'>
-                        <h1 className = 'title'>
-                            HATS
-                        </h1>
-                        <span className = 'subtitle'>
-                          SHOP NOW  
-                            </span>
-                    </div>
-                </div>
-
-
-            </div>
+            <Directory />
         </div>
     )
 }
