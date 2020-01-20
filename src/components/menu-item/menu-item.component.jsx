8e43157af14b4a927a39070f0acdc38d556f122c
Gabmeister <gabriel@gabrielhorvat.com>
2020-01-20 10:27:32 +0100

    menu item zoom in effect

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -7,6 +7,11 @@ const MenuItem = ({title, imageUrl, size}) => (
       backgroundImage: `url(${imageUrl})`
     }}
      className = {`${size} menu-item`}>
+
+<div className = 'background-image' style = {{
+        backgroundImage: `url(${imageUrl})`
+}}> 
+</div>   
     <div className = 'content'>
         <h1 className = 'title'>
              {title.toUpperCase()}
