43b8484c97126373844ea687865d47f640fcc592
Gabmeister <gabriel@gabrielhorvat.com>
2020-01-20 15:21:25 +0100

    fix bug

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import {Switch, Route} from 'react-router-dom'
-import ShopPage from './pages/shop/shop.component '
+import ShopPage from './pages/shop/shop.component'
 
 const HatsPage = () => (
   <div>
