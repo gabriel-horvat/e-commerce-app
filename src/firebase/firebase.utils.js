f097cabdc982787f23279e931050741604288c4b
Gabmeister <gabriel@gabrielhorvat.com>
2020-01-22 13:09:08 +0100

    firebase config

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,27 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const config = {
+
+        apiKey: "AIzaSyCnWTNP545ojb4Py_Y91iqASydw5_A4yBU",
+        authDomain: "crown-db-28869.firebaseapp.com",
+        databaseURL: "https://crown-db-28869.firebaseio.com",
+        projectId: "crown-db-28869",
+        storageBucket: "crown-db-28869.appspot.com",
+        messagingSenderId: "803209002060",
+        appId: "1:803209002060:web:60b8317cf639c210f0c8e9",
+        measurementId: "G-FHFN42WJ27"
+
+}
+
+firebase.initializeApp(config)
+
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
+
+const provider = new firebase.auth.GoogleAuthProvider()
+provider.setCustomParameters({prompt: 'select_account'})
+export const signInWithGoogle = () => auth.signInWithPopup(provider)
+
+export default firebase
