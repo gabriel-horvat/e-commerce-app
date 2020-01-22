b2362c8253fb0d59184107c82c68894a0970d19e
Gabmeister <gabriel@gabrielhorvat.com>
2020-01-22 14:04:01 +0100

    google sign in button + styling

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -55,9 +55,11 @@ class SignIn extends React.Component {
                    value = {password}
                     required />
 
+                <div className="buttons">
                 <CustomButton type="submit"> Sign in </CustomButton>
-                <CustomButton onClick={signInWithGoogle}> Sign in with Google </CustomButton>
-
+                <CustomButton onClick={signInWithGoogle} isGoogleSignIn> Sign in with Google </CustomButton>
+                </div>
+                
                 </form>
             </div>
         )
