1230783ebccd0f8c7e35b5498029f4ada77cbf92
Gabmeister <gabriel@gabrielhorvat.com>
2020-01-22 13:16:34 +0100

    add signInWithGoogle

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import {Switch, Route} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import {auth} from './firebase/firebase.utils'
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null
+
+  componentDidMount(){
+    auth.onAuthStateChanged(user =>{
+      this.setState({ currentUser: user })
+      console.log(user);
+      
+    } )
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth()
+  }
+
+  render() 
+  {
+    return (
     <div className="App">
       <Header />
       <Switch>
@@ -16,7 +41,8 @@ function App() {
       <Route exact path = '/signin' component ={SignInAndSignUpPage} />
       </Switch>
     </div>
-  );
+  )
+}
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import FormInput from '../form-input/form-input.component'
 import './sign-in.styles.scss'
 import CustomButton from '../custom-button/custom-button.component'
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 
 class SignIn extends React.Component {
     constructor(props){
@@ -55,6 +56,8 @@ class SignIn extends React.Component {
                     required />
 
                 <CustomButton type="submit"> Sign in </CustomButton>
+                <CustomButton onClick={signInWithGoogle}> Sign in with Google </CustomButton>
+
                 </form>
             </div>
         )
