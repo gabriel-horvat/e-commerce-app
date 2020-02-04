a8c1b5856a17d2afbd54157dfc35bd16522faee4
Gabmeister <gabriel@gabrielhorvat.com>
2020-02-04 16:00:22 +0100

    toggle cartDropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 
 import './header.styles.scss';
 
-const Header = ({currentUser}) => (
+const Header = ({currentUser, hidden}) => (
   <div className='header'>
     <Link className='logo-container' to='/'>
       <Logo className='logo' />
@@ -34,12 +34,14 @@ const Header = ({currentUser}) => (
       }
     <CartIcon />      
     </div>
-    <CartDropdown/>
+    {  hidden ? null : <CartDropdown/>}
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: {currentUser}, 
+cart: {hidden}
+ }) => ({
+  currentUser, hidden
 })
 
 export default connect(mapStateToProps)(Header);
