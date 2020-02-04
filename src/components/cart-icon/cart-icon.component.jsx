3656d740c8406ac1eaae5fab03b3b8ae30af8fde
Gabmeister <gabriel@gabrielhorvat.com>
2020-02-04 15:54:49 +0100

    redux for cart component

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,20 +1,19 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import {auth} from '../../firebase/firebase.utils'
 import { connect } from 'react-redux'
 import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import {toggleCartHidden} from '../../redux/cart/cart.actions'
 import './cart-icon.styles.scss';
 
-const CartIcon = () => (
-  <div className='cart-icon'>
+const CartIcon = ({toggleCartHidden}) => (
+  <div className='cart-icon' onClick ={toggleCartHidden} >
    <ShoppingIcon className="shopping-icon"/>
     <span className="item-count">0
     </span>
   </div>
 );
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: ()=> dispatch(toggleCartHidden())
 })
 
-export default connect(mapStateToProps)(CartIcon);
+export default connect(null, mapDispatchToProps)(CartIcon);
