6078dc903eb3ad6edd399b4cda993c56ec340652
Gabmeister <gabriel@gabrielhorvat.com>
2020-02-04 13:13:52 +0100

    redirect to home from signin

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
-import {Switch, Route} from 'react-router-dom'
+import {Switch, Route, Redirect} from 'react-router-dom'
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
@@ -40,21 +40,30 @@ class App extends Component {
   }
 
   render() {
+    const {currentUser} = this.props
     return (
     <div className="App">
       <Header />
       <Switch>
       <Route exact path = '/' component = {HomePage} />
       <Route exact path = '/shop' component ={ShopPage} />
-      <Route exact path = '/signin' component ={SignInAndSignUpPage} />
+      <Route exact path = '/signin' render = {() =>
+      currentUser ? (
+        <Redirect to = '/' />
+      ) : (<SignInAndSignUpPage />)
+    }  />
       </Switch>
     </div>
   )
 }
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
