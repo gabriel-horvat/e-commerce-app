0f1b22a5d36c5d10d83cb6d9ffefc54866496b3a
Gabmeister <gabriel@gabrielhorvat.com>
2020-02-04 13:02:34 +0100

    rootReducer

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 // import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
-import { store } from "./redux/store";
+import  store  from "./redux/store";
 
 ReactDOM.render(
 <Provider store = {store}>
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,6 @@
 import {createStore, applyMiddleware} from 'redux'
 import logger from 'redux-logger'
-import {  rootReducer} from "./root-reducer";
+import rootReducer from './root.reducer'
 
 const middlewares = [logger]
 
