2519ab066ea32782a4e2a340d14c327cdace627d
Gabmeister <gabriel@gabrielhorvat.com>
2020-02-04 13:01:54 +0100

    add redux support for header and app

--- src/App.js
+++ src/App.js
@@ -6,28 +6,22 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
+import { connect } from 'react-redux';
+import {setCurrentUser} from './redux/user/user.actions'
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unsubscribeFromAuth = null
 
   componentDidMount(){
+    const {setCurrentUser} = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth)
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
+          setCurrentUser({
               id: snapShot.id,
               ...snapShot.data()
-            }
           }, ()=>{
             console.log(this.state);
           })
@@ -35,7 +29,7 @@ class App extends Component {
          
       }
       else {
-        this.setState({ currentUser: userAuth})
+        setCurrentUser(userAuth)
       }
     })
 
@@ -46,7 +40,6 @@ class App extends Component {
   }
 
   render() {
-    const {currentUser} = this.state
     return (
     <div className="App">
       <Header />
@@ -60,4 +53,8 @@ class App extends Component {
 }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import {auth} from '../../firebase/firebase.utils'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
