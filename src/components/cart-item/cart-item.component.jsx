43a8cdbd8a0e8877fb295c632021ecedfa63dae3
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-06 12:59:03 +0100

    create cartItem component

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import './cart-item.styles.scss'
+import React from 'react';
 
-const CartItem = ({ item: {imageUrl, price, name, quantity}}) => (
-    <div className="cart-item">
-        <img src="{imageUrl}" alt="item" className="item"/>
-        <div className="item-details">
-            <span className="name">
-                {name}
-            </span>
-            <span className="name">
-                {quantity} x ${price}
-            </span>
-        </div>
+import './cart-item.styles.scss';
+
+const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
+  <div className='cart-item'>
+    <img src={imageUrl} alt='item' />
+    <div className='item-details'>
+      <span className='name'>{name}</span>
+      <span className='price'>
+        {quantity} x ${price}
+      </span>
     </div>
-)
+  </div>
+);
 
-export default CartItem
+export default CartItem;
