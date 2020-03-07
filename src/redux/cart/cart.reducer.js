3f64b2671dc2cdc46accd38afcf4c8852ad3f9ed
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-07 22:28:11 +0100

    removeItem action, reducer, types

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,5 +1,5 @@
 import {cartActionTypes} from './cart.types'
-import {addItemToCart} from './cart.utils'
+import {addItemToCart, removeItemFromCart} from './cart.utils'
 import { clearItemFromCart } from './cart.actions'
 import CartItem from '../../components/cart-item/cart-item.component'
 
@@ -20,6 +20,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
                     ...state,
                     cartItems: addItemToCart(state.cartItems, action.payload)
                 }
+                case cartActionTypes.REMOVE_ITEM:
+                    return {
+                        ...state,
+                        cartItems: removeItemFromCart(state.cartItems, action.payload )
+                    }
                 case cartActionTypes.CLEAR_ITEM_FROM_CART:
                     return {
                         ...state,
