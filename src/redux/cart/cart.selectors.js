17f74782cb9faf7f4e6d1e66e8325fa1a62fdc09
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-07 20:19:23 +0100

    cartTotal selector

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -18,3 +18,11 @@ export const selectCartItemsCount = createSelector(
     cartItems.reduce(
         (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity, 0)
 )
+
+export const selectCartTotal = createSelector(
+    [selectCartItems],
+    cartItems =>
+    cartItems.reduce(
+        (accumalatedQuantity, cartItem) => 
+        accumalatedQuantity + cartItem.quantity * cartItem.price, 0)
+)
