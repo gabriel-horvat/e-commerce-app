23e3de9c8ddf7975068740e3180ead1da93b875a
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-07 19:43:41 +0100

    empty cart message

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -8,19 +8,21 @@ import {createStructuredSelector} from 'reselect'
 
 
 const CartDropdown = ({cartItems}) => (
-    <div className="cart-dropdown">
-        <div className="cart-items">
-            {
-                cartItems.map(cartItem => (<CartItem
-                     key = {cartItem.id}
-                     item = {cartItem} />))
-            }
-            </div>
-            <CustomButton>
-                GO TO CHECKOUT
-            </CustomButton>
+    <div className='cart-dropdown'>
+      <div className='cart-items'>
+        {cartItems.length ? (
+          cartItems.map(cartItem => (
+            <CartItem key={cartItem.id} item={cartItem} />
+          ))
+        ) : (
+          <span className='empty-message'>Your cart is empty</span>
+        )}
+      </div>
+      <CustomButton>
+        GO TO CHECKOUT
+      </CustomButton>
     </div>
-)
+  );
 
 const mapStateToProps = createStructuredSelector({
     cartItems: selectCartItems
