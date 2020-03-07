8d4bc6e9626cbde6d249d47a167d8c0c81d0e2f4
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-07 19:42:17 +0100

    fix bug

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
 import { connect } from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions'
-import {selectCurrentUser} from '../../redux/user/user.selectors'
+import {selectCurrentUser} from './redux/user/user.selectors'
 import {createStructuredSelector} from 'reselect'
 
 class App extends Component {
