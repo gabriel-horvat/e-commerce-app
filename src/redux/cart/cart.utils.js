53e5445e211bd6ef0d60d2a2082edb282af729cb
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-07 22:18:15 +0100

    removeItemFromCart helper

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   
     return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
   };
+
+  export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartitems.find(
+      cartItem => cartItem.id == cartItemToRemove.id
+    )
+
+    if (existingCartItem.quantity == 1) {
+      return cartItems.filter(cartItem.id != cartItemToRemove.id)
+    }
+
+    return cartItems.map(
+      cartItem => cart.id == cartItemToRemove.id 
+      ? 
+      {...cartItem, quantity: cartItem.quantity -1 }
+      : cartItem
+    )
+  }
