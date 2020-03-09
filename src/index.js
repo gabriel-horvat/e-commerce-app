856b872f30d66caddf59ed25c8ad73420b902788
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-09 18:53:11 +0100

    integrate redux-persist

--- src/index.js
+++ src/index.js
@@ -5,12 +5,15 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from 'react-router-dom'
 import {Provider} from 'react-redux'
-import  store  from "./redux/store";
+import  {store, persistor}  from "./redux/store";
+import {PersistGate} from 'redux-persist/integration/react'
 
 ReactDOM.render(
 <Provider store = {store}>
 <BrowserRouter>
+<PersistGate persistor = {persistor}> 
 <App />
+</PersistGate>
 </BrowserRouter>
 </Provider>, document.getElementById('root'));
 
