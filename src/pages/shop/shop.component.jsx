dfef6adb9052082d9a66b50a36cabecddd563bd1
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-12 11:29:25 +0100

    refactor shop component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,15 @@
 import React from 'react'
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import {selectCollections} from '../../redux/shop/shop.selectors'
+import {Route} from 'react-router-dom'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
+import CategoryPage from '../category/category.component'
 
-const ShopPage = ({collections}) => 
-(
+const ShopPage = ({match}) => {
+
+return(
             <div className = "shop-page">
-               <CollectionsOverview />
+<Route exact path = {`${match.path}`} component ={CollectionsOverview} />
+<Route path = {`${match.path}/:categoryId`} component ={CategoryPage} />
             </div>
-        )
-
-        const mapStateToProps = createStructuredSelector({
-            collections: selectCollections
-          });
+        )}
           
-          export default connect(mapStateToProps)(ShopPage);
+          export default ShopPage
