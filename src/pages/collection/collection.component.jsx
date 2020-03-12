08ccb18bc97f2610245dd8adeb933d798d3759fb
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-12 12:51:14 +0100

    basic CollectionPage

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,22 +1,25 @@
 import React from "react";
 import { connect } from "react-redux";
 
-import CollectionItem from "../../components/collection-item/collection-item.component";
+// import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
 import "./collection.styles.scss";
 
 const CollectionPage = ({ collection }) => {
-  const { title, items } = collection;
+  console.log(collection);
+
+  // const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2 className="title">{title}</h2>
+      CollectionPage
+      {/* <h2 className="title">{title}</h2>
       <div className="items">
         {items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div>
+      </div> */}
     </div>
   );
 };
