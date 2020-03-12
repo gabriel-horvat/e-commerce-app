ce3b95af7a160db7d8cf45acff2b438642b17958
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-12 11:30:20 +0100

    nested routing for shop page

---
+++ src/pages/category/category.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import CollectionItem from '../../components/collection-item/collection-item.component'
+import './category.styles.scss'
+
+const CategoryPage = ({match}) => {
+console.log(match);
+
+return(
+    <div className="category">
+        <h1>CategoryPage</h1>
+    </div>
+)}
+
+export default CategoryPage
