60d3e8b975bc7ce813a22c6243f52669daa75a3a
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-12 12:39:39 +0100

    collectionPage

--- src/App.js
+++ src/App.js
@@ -1,73 +1,74 @@
-import React, { Component } from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component'
-import CheckoutPage from './pages/checkout/checkout.component'
-import {Switch, Route, Redirect} from 'react-router-dom'
-import ShopPage from './pages/shop/shop.component'
-import Header from './components/header/header.component'
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
-import {auth, createUserProfileDocument} from './firebase/firebase.utils'
-import { connect } from 'react-redux';
-import {setCurrentUser} from './redux/user/user.actions'
-import {selectCurrentUser} from './redux/user/user.selectors'
-import {createStructuredSelector} from 'reselect'
+import React, { Component } from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+import { Switch, Route, Redirect } from "react-router-dom";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
+import { selectCurrentUser } from "./redux/user/user.selectors";
+import { createStructuredSelector } from "reselect";
 
 class App extends Component {
+  unsubscribeFromAuth = null;
 
-  unsubscribeFromAuth = null
-
-  componentDidMount(){
-    const {setCurrentUser} = this.props
+  componentDidMount() {
+    const { setCurrentUser } = this.props;
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth)
+        const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          setCurrentUser({
+          setCurrentUser(
+            {
               id: snapShot.id,
               ...snapShot.data()
-          }, ()=>{
-            console.log(this.state);
-          })
-        })
-         
+            },
+            () => {
+              console.log(this.state);
+            }
+          );
+        });
+      } else {
+        setCurrentUser(userAuth);
       }
-      else {
-        setCurrentUser(userAuth)
-      }
-    })
-
+    });
   }
 
   componentWillUnmount() {
-    this.unsubscribeFromAuth()
+    this.unsubscribeFromAuth();
   }
 
   render() {
-    const {currentUser} = this.props
+    const { currentUser } = this.props;
     return (
-    <div className="App">
-      <Header />
-      <Switch>
-      <Route exact path = '/' component = {HomePage} />
-      <Route exact path = '/shop' component ={ShopPage} />
-      <Route exact path = '/checkout' component ={CheckoutPage} />
-      <Route exact path = '/signin' render = {() =>
-      currentUser ? (
-        <Redirect to = '/' />
-      ) : (<SignInAndSignUpPage />)
-    }  />
-      </Switch>
-    </div>
-  )
-}
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              currentUser ? <Redirect to="/" /> : <SignInAndSignUpPage />
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
-})
+});
 
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-})
+});
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
-import {Route} from 'react-router-dom'
-import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
-import CollectionPage from '../category/collection.component'
+import React from "react";
+import { Route } from "react-router-dom";
+import CollectionsOverview from "../../components/collections-overview/collections-overview.component";
+import CollectionPage from "../collection/collection.component";
 
-const ShopPage = ({match}) => {
+const ShopPage = ({ match }) => (
+  <div className="shop-page">
+    <Route exact path={`${match.path}`} component={CollectionsOverview} />
+    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+  </div>
+);
 
-return(
-            <div className = "shop-page">
-<Route exact path = {`${match.path}`} component ={CollectionsOverview} />
-<Route path = {`${match.path}/:collectionId`} component ={CollectionPage} />
-            </div>
-        )}
-          
-          export default ShopPage
-          
-          
+export default ShopPage;
