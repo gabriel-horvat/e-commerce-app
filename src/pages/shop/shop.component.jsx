739aa073f3f20956486e66dd8a86d0441a09f835
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-12 12:29:24 +0100

    collection selector + routing

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,15 +1,17 @@
 import React from 'react'
 import {Route} from 'react-router-dom'
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component'
-import CategoryPage from '../category/category.component'
+import CollectionPage from '../category/collection.component'
 
 const ShopPage = ({match}) => {
 
 return(
             <div className = "shop-page">
 <Route exact path = {`${match.path}`} component ={CollectionsOverview} />
-<Route path = {`${match.path}/:categoryId`} component ={CategoryPage} />
+<Route path = {`${match.path}/:collectionId`} component ={CollectionPage} />
             </div>
         )}
           
           export default ShopPage
+          
+          
