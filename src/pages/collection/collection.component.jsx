a429ded2b4142045b4f0f8fc797288cad3efa71e
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-13 16:29:21 +0100

    normalized data

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 
-// import CollectionItem from "../../components/collection-item/collection-item.component";
+import CollectionItem from "../../components/collection-item/collection-item.component";
 
 import { selectCollection } from "../../redux/shop/shop.selectors";
 
@@ -10,16 +10,16 @@ import "./collection.styles.scss";
 const CollectionPage = ({ collection }) => {
   console.log(collection);
 
-  // const { title, items } = collection;
+  const { title, items } = collection;
   return (
     <div className="collection-page">
       CollectionPage
-      {/* <h2 className="title">{title}</h2>
+      <h2 className="title">{title}</h2>
       <div className="items">
         {items.map(item => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 };
