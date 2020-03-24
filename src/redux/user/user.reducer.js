3ee7f69f8e4705ed3fdd4c168ba07e3189454496
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-24 20:49:23 +0100

    npm update

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,31 +1,17 @@
-import UserActionTypes from "./user.types";
+import { userActionTypes } from "./user.types";
 
 const INITIAL_STATE = {
-  currentUser: null,
-  error: null
+  currentUser: null
 };
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case UserActionTypes.SIGN_IN_SUCCESS:
+    case userActionTypes.SET_CURRENT_USER:
       return {
         ...state,
-        currentUser: action.payload,
-        error: null
+        currentUser: action.payload
       };
-    case UserActionTypes.SIGN_OUT_SUCCESS:
-      return {
-        ...state,
-        currentUser: null,
-        error: null
-      };
-    case UserActionTypes.SIGN_IN_FAILURE:
-    case UserActionTypes.SIGN_OUT_FAILURE:
-    case UserActionTypes.SIGN_UP_FAILURE:
-      return {
-        ...state,
-        error: action.payload
-      };
+
     default:
       return state;
   }
