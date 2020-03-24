8c3651851dbebfd66f609da1dff4e050916e48ec
Gabmeister <gabriel@gabrielhorvat.com>
2020-03-24 16:03:46 +0100

    clean up code

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -19,10 +19,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   const userRef = firestore.doc(`users/${userAuth.uid}`);
-
   const snapShot = await userRef.get();
-
-  // console.log(snapShot);
 
   if (!snapShot.exists) {
     const { displayName, email } = userAuth;
