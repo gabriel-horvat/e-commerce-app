2f293682d35ef7eb531e2169d31e236bd0cedb50
Gabmeister <gabriel@gabrielhorvat.com>
2020-04-18 18:04:50 +0200

    reset header - currentUser still lost

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,72 +9,35 @@ import { createStructuredSelector } from "reselect";
 import "./header.styles.scss";
 import { selectCartHidden } from "./../../redux/cart/cart.selectors";
 import { selectCurrentUser } from "../../redux/user/user.selectors";
-// import { withRouter } from "react-router-dom";
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
+const Header = ({ currentUser, hidden }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
+    </Link>
+    <span className="header-title"> The G Store ✌🏻</span>
+    <div className="options">
+      <Link className="option" to="/shop">
+        SHOP
+      </Link>
 
-    this.state = {
-      currentUser: "Joe Blow",
-    };
-  }
-  handleLogout = async (event) => {
-    event.preventDefault();
-
-    try {
-      await auth.signOut();
-      alert("You're successfully logged out. See ya again soon!");
-      // this.props.history.push("/");
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
-  render() {
-    const { currentUser, hidden } = this.state;
-
-    return (
-      <div className="header">
-        <Link className="logo-container" to="/">
-          <Logo className="logo" />
+      {currentUser ? (
+        <Link className="option" onClick={() => auth.signOut()} to="/">
+          SIGN OUT
         </Link>
-        <span className="header-title">
-          {" "}
-          The G Store
-          <span className="yo-emoji" role="img" aria-label="peace-sign">
-            ✌🏻
-          </span>
-        </span>
-        <span className="navbar-text mr-3">
-          {" "}
-          {console.log(currentUser)}
-          {currentUser ? `Welcome ${currentUser}` : ""}
-        </span>
-        <div className="options">
-          <Link className="option" to="/shop">
-            SHOP
-          </Link>
-
-          {currentUser ? (
-            <Link className="option" to="/" onClick={this.handleLogout}>
-              SIGN OUT
-            </Link>
-          ) : (
-            <Link className="option" to="/signin">
-              SIGN IN
-            </Link>
-          )}
-          <CartIcon />
-        </div>
-        {hidden ? null : <CartDropdown />}
-      </div>
-    );
-  }
-}
+      ) : (
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
+      )}
+      <CartIcon />
+    </div>
+    {hidden ? null : <CartDropdown />}
+  </div>
+);
 
 const mapStateToProps = createStructuredSelector({
-  // currentUser: selectCurrentUser,
+  currentUser: selectCurrentUser,
   hidden: selectCartHidden,
 });
 
