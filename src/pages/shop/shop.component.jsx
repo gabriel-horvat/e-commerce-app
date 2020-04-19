959d7b8e54df594b073fba92f606e4888d87ea20
Gabmeister <gabriel@gabrielhorvat.com>
2020-04-19 17:38:49 +0200

    fix compDidMount in shopComponent

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -23,13 +23,13 @@ class ShopPage extends React.Component {
   componentDidMount() {
     const { updateCollections } = this.props;
     const collectionRef = firestore.collection("collections");
-    this.unsubscribeFromSnapshot = collectionRef.onSnapshot(
-      async (snapshot) => {
-        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
-        updateCollections(collectionsMap);
-      }
-    );
-    this.setState({ loading: false });
+
+    collectionRef.get().then((snapshot) => {
+      const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
+      this.setState({ loading: false });
+    });
+
     // const { fetchCollectionsStart } = this.props;
     // fetchCollectionsStart();
   }
