4235e5d2b55d8210f6247ba36c5246f408f30e5b
Gabmeister <gabriel@gabrielhorvat.com>
2020-04-19 16:32:52 +0200

    collectionComp WIP - start on observables

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -13,7 +13,7 @@ const CollectionPage = ({ collection }) => {
     <div className="collection-page">
       <h2 className="title">{title}</h2>
       <div className="items">
-        {items.map(item => (
+        {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
       </div>
@@ -22,7 +22,7 @@ const CollectionPage = ({ collection }) => {
 };
 
 const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
+  collection: selectCollection(ownProps.match.params.collectionId)(state),
 });
 
 export default connect(mapStateToProps)(CollectionPage);
