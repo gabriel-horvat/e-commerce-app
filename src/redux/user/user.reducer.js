5097bd6433c8035196d77e9534158f1b4a64019f
Gabmeister <gabriel@gabrielhorvat.com>
2020-04-21 14:48:27 +0200

    sign in into sagas user reducer

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -7,12 +7,19 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case userActionTypes.SET_CURRENT_USER:
+    case userActionTypes.GOOGLE_SIGN_IN_SUCCESS:
+    case userActionTypes.EMAIL_SIGN_IN_SUCCESS:
       return {
         ...state,
         currentUser: action.payload,
+        error: null,
       };
-
+    case userActionTypes.GOOGLE_SIGN_IN_FAILURE:
+    case userActionTypes.EMAIL_SIGN_IN_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+      };
     default:
       return state;
   }
