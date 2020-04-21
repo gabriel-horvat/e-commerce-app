47fbaed0c902aec9047c607ecfd8e5bffb0ab91d
Gabmeister <gabriel@gabrielhorvat.com>
2020-04-21 14:48:18 +0200

    sign in into sagas user actions

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,6 +1,35 @@
 import { userActionTypes } from "./user.types";
 
-export const setCurrentUser = user => ({
+export const setCurrentUser = (user) => ({
   type: userActionTypes.SET_CURRENT_USER,
-  payload: user
+  payload: user,
+});
+
+export const googleSignInStart = () => ({
+  type: userActionTypes.GOOGLE_SIGN_IN_START,
+});
+
+export const googleSignInSuccess = (user) => ({
+  type: userActionTypes.GOOGLE_SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+export const googleSignInFailure = (error) => ({
+  type: userActionTypes.GOOGLE_SIGN_IN_FAILURE,
+  payload: error,
+});
+
+export const emailSignInStart = (emailAndPassword) => ({
+  type: userActionTypes.EMAIL_SIGN_IN_START,
+  payload: emailAndPassword,
+});
+
+export const emailSignInSuccess = (user) => ({
+  type: userActionTypes.EMAIL_SIGN_IN_SUCCESS,
+  payload: user,
+});
+
+export const emailSignInFailure = (error) => ({
+  type: userActionTypes.EMAIL_SIGN_IN_FAILURE,
+  payload: error,
 });
