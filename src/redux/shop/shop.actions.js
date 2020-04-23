0e971e99091fc6c0f7b3a0837c12aeb37d4fa102
Gabmeister <gabriel@gabrielhorvat.com>
2020-04-23 17:46:11 +0200

    fix fetchCollections saga bug

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,4 +1,8 @@
 import ShopActionTypes from "./shop.types";
+import {
+  firestore,
+  convertCollectionsSnapshotToMap,
+} from "../../firebase/firebase.utils";
 
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START,
@@ -15,5 +19,16 @@ export const fetchCollectionsFailure = (errorMessage) => ({
 });
 
 export const fetchCollectionsStartAsync = () => {
-  return (dispatch) => {};
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart());
+
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+  };
 };
--- src/redux/shop/shop.saga.js
+++ src/redux/shop/shop.saga.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from "redux-saga/effects";
+import { takeLatest, call, put, all } from "redux-saga/effects";
 import ShopActionTypes from "./shop.types";
 
 import {
@@ -30,4 +30,8 @@ export function* fetchCollectionsStart() {
     ShopActionTypes.FETCH_COLLECTIONS_START,
     fetchCollectionsAsync
   );
+}
+
+export function* shopSagas() {
+  yield all([call(fetchCollectionsStart)]);
 }
