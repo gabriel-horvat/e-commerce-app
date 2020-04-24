0ad64e44c319a75f69e281bc8008375a10087d93
Gabmeister <gabriel@gabrielhorvat.com>
2020-04-24 15:16:02 +0200

    cart saga -clear cart

--- src/redux/root-saga.js
+++ src/redux/root-saga.js
@@ -1,7 +1,8 @@
 import { all, call } from "redux-saga/effects";
 import { userSagas } from "./user/user-saga";
 import { shopSagas } from "./shop/shop.saga";
+import { cartSagas } from "./cart/cart.saga";
 
 export default function* rootSaga() {
-  yield all([call(userSagas), call(shopSagas)]);
+  yield all([call(userSagas), call(shopSagas), call(cartSagas)]);
 }
