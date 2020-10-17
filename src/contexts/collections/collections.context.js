0e8e0fe574a4f1a9667daa597fb5828697b92b14
Gabmeister <gabriel@gabrielhorvat.com>
2020-10-17 21:49:52 +0200

    collectionsContext component

--- src/contexts/collections/collections.context.js
+++ src/contexts/collections/collections.context.js
@@ -1,3 +1,6 @@
 import { createContext } from "react";
+import SHOP_DATA from "./shop.data";
 
-const CollectionsContext = createContext();
+const CollectionsContext = createContext(SHOP_DATA);
+
+export default CollectionsContext;
