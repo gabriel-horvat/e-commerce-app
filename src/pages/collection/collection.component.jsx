84ddbace4484fea610cee06ef2b1abde48d8bea7
Gabmeister <gabriel@gabrielhorvat.com>
2020-10-17 22:13:25 +0200

    refactor collection component useContext hook

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import { connect } from "react-redux";
-
+import React, { useContext } from "react";
 import CollectionItem from "../../components/collection-item/collection-item.component";
-
-import { selectCollection } from "../../redux/shop/shop.selectors";
-
 import "./collection.styles.scss";
+import CollectionsContext from "../../contexts/collections/collections.context";
 
-const CollectionPage = ({ collection }) => {
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
   const { title, items } = collection;
   return (
     <div className="collection-page">
@@ -21,8 +19,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state),
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
