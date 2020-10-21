c38697a1f14954ba2ad1a42fe19ecb4153373fd5
Gabmeister <gabriel@gabrielhorvat.com>
2020-10-21 11:10:57 +0200

    react profiler

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import Directory from '../../components/directory/directory.component'
+import React, { Profiler } from "react";
+import Directory from "../../components/directory/directory.component";
 
 export const HomePage = () => {
-    return (
-        <div className = 'homepage'>
-            <Directory />
-        </div>
-    )
-}
+  return (
+    <div className="homepage">
+      <Profiler
+        id="directory"
+        onRender={(id, phase, actualDuration) =>
+          console.log({
+            id,
+            phase,
+            actualDuration,
+          })
+        }
+      >
+        <Directory />
+      </Profiler>
+    </div>
+  );
+};
 
-export default HomePage 
+export default HomePage;
